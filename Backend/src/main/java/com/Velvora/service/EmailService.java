994e7f4513299f40aad205e915a4c436a56d1a86
Yashu Youwaraj <amanyasu@gmail.com>
2026-07-15 03:26:48 +0530

    Otp fix for backend

--- Backend/src/main/java/com/Velvora/service/EmailService.java
+++ Backend/src/main/java/com/Velvora/service/EmailService.java
@@ -1,14 +1,15 @@
 package com.Velvora.service;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
